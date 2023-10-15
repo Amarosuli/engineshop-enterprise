6c47112140b23d8bf272067c95eccaf00795d7fa
Amarosuli <vuvlnvw@gmail.com>
2023-10-15 21:07:51 +0700

    update dependencies

---
+++ src/lib/components/commons/Table/EditableCell.svelte
@@ -0,0 +1,45 @@
+<script>
+   import {fade} from 'svelte/transition'
+   import {Text} from '$lib/components'
+   export let id
+   export let name
+   export let value = ''
+   export let data
+
+   let isEditing = false
+   let inputElement
+
+   $: if (isEditing) {
+      inputElement?.focus()
+   }
+
+  $: $data[id][name] = value
+
+function handleKeyDown(event) {
+   if (event.key === 'Escape' || event.keyCode === 27) isEditing = false
+}
+</script>
+
+{#if !isEditing}
+<span in:fade on:click={()=> isEditing = true}>{value}</span>
+{:else}
+<div>
+   <input in:fade type="text" bind:this={inputElement} bind:value on:blur={() => isEditing = false}>
+</div>
+{/if}
+
+
+<svelte:document on:keydown={handleKeyDown} />
+
+<style lang="postcss">
+   div {
+		@apply flex w-full flex-col space-y-1;
+	}
+	span {
+		@apply flex justify-start items-center w-full h-full text-xs italic text-red-600;
+	}
+
+   input {
+		@apply h-10 border-b border-slate-200 px-4 py-2 text-slate-700 transition-colors ease-out hover:border-sky-200 hover:bg-slate-200 focus:border-sky-500 focus:bg-slate-50 focus:outline-none;
+	}
+</style>
--- src/routes/app/delivery-note/TableInput.svelte
+++ src/routes/app/delivery-note/TableInput.svelte
@@ -1,31 +1,65 @@
 <script>
+   import { createRender } from 'svelte-headless-table';
+
+   import EditableCell from '$lib/components/commons/Table/EditableCell.svelte'
 	import { SuperTable } from '$lib/components/commons/Table/tableClass.js';
 	import { writable } from 'svelte/store';
 	import { Render, Subscribe } from 'svelte-headless-table';
-
-	let data$ = new writable([
-		{
+   import {Button} from '$lib/components'
+  
+   let templateData = {
 			quantity: '',
 			description: '',
 			remark: ''
 		}
-	]);
-	let dataCol = [
+
+   let bucketData = [{id: 0, ...templateData}]
+   
+   let data$ = new writable([])
+
+   $: $data$ = bucketData
+
+   function addRow() {
+      let newId = $data$.length
+      let lastValue = $data$
+      bucketData = [...lastValue, {id: newId, ...templateData}]
+   }
+
+   let dataCol = [
 		{
 			header: 'Quantity',
-			accessor: 'quantity'
+			accessor: 'quantity',
+         cell:  ({row,column, value}) => {
+            let name = column.id
+            const {data} = row.state
+           let id = row.original.id
+               return createRender(EditableCell, {id, name, value, data})
+            }
 		},
 		{
 			header: 'Description',
-			accessor: 'description'
+			accessor: 'description',
+         cell:  ({row,column, value}) => {
+            let name = column.id
+              const {data} = row.state
+           let id = row.original.id
+               return createRender(EditableCell, {id, name, value, data})
+            }
 		},
 		{
 			header: 'Remark',
-			accessor: 'remark'
+			accessor: 'remark',
+         cell:  ({row,column, value}) => {
+            let name = column.id
+              const {data} = row.state
+           let id = row.original.id
+               return createRender(EditableCell, {id, name, value, data})
+            }
 		}
 	];
 	const table = new SuperTable(data$, dataCol, { rowSelector: false, sort: false });
 	let { headerRows: Hr, rows: Br, tableAttrs: T, tableBodyAttrs: B, pluginStates: Ps } = table.init;
+   let { exportedData } = Ps.export;
 </script>
 
 <table {...$T}>
@@ -62,3 +96,4 @@
 		{/each}
 	</tbody>
 </table>
+<Button.Event title='Add Row' on:Event={addRow}/>
